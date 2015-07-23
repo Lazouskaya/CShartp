1198dfce32d18c769648e8fc8ab990e50c6f8b6b
Viktoryia Lazouskaya <Viktoryia_Lazouskaya@epam.com>
2015-07-23 18:19:02 +0300

    add lib and task

---
+++ NodMethods/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodMethods
+{
+    public static class Nod
+    {
+        public static int Evclidean(int firstNum, int secondNum)
+        {
+            checkArguments(firstNum,secondNum);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int remainder = 1;
+            while (firstNum % secondNum != 0)
+            {
+                remainder=firstNum%secondNum;
+                firstNum = secondNum;
+                secondNum = remainder;
+            }
+            getExecutionTime(stopWatch);
+            return remainder;
+        }
+
+        public static int Evclidean(params int[] numbers)
+        {
+            checkArguments(numbers);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int remainder = 1;
+            while (firstNum % secondNum != 0)
+            {
+                remainder = firstNum % secondNum;
+                firstNum = secondNum;
+                secondNum = remainder;
+            }
+            getExecutionTime(stopWatch);
+            return remainder;
+        }
+
+        private static void getExecutionTime(Stopwatch stopWatch)
+        {
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+
+        private static void swap(int number1, int number2)
+        {
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
+
+        private static void checkArguments(int firstNum, int secondNum)
+        {
+            if (firstNum == 0 && secondNum == 0 || firstNum < 0 || secondNum < 0)
+            {
+                throw new ArgumentException();
+            }
+            if (firstNum < secondNum)
+            {
+                swap(firstNum, secondNum);
+            }
+        }
+    }
+}
---
+++ packages/NQUnit.NUnit.1.0.5/readme.txt
@@ -0,0 +1,22 @@
+NQUnit.NUnit
+------------
+
+Example project to get you up and running with NQUnit using the NUnit test runner quickly.
+
+Getting started
+===============
+
+1. Install the NQUnit.NUnit package (already done)
+2. Set all of the files within the JavaScriptTests\ and JavaScriptTests\Scripts folders to "Copy if newer" so they get copied to the output directory and can be found by the test runner
+3. Set the "Embed Interop Types" setting to false for the "Interop.SHDocVw" reference for the project otherwise you will get a "Unhandled Exception: System.IO.FileLoadException: Could not load file or assembl y 'Interop.SHDocVw...'" error
+4. Run the tests using an NUnit runner of choice
+
+Addinga new test
+================
+
+Copy/rename JavaScriptTests\blank.html and JavaScriptTests\blank.js to create tests. Add as many .html files as you like to the JavaScriptTests folder to add new tests (they will autoamtically be picked up) - just remember to set them all as "Copy if newer".
+
+Advanced usage / Using other test runners
+=========================================
+
+Inspect the files in JavaScriptTests\NQUnit to tweak the integration and to see how you might do it for different test runners.
